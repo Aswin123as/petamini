234368708b0a7ea8aa2e428d9d7bbaa27f739114
Aswin123as <aswinspas12@gmail.com>
2025-09-29 15:24:12 +0530

    added to the route

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -36,6 +36,9 @@ export const IndexPage: FC = () => {
           <Link to="/theme-params">
             <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
           </Link>
+          <Link to="/pokemon">
+            <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
+          </Link>
         </Section>
       </List>
     </Page>
